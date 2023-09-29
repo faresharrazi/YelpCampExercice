fdf2e2847943657e80cf37a3fff6dd7a6bbba22a
Harrazi <m.fares.harrazi@gmail.com>
2023-09-29 12:11:50 +0300

    updates

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const methodOverride = require ('method-override');
 const Campgound = require ('./models/campground');
 const ejsMate = require ('ejs-mate');
 
+const catchAsync = require('./utilities/catchAsync');
+
 mongoose.connect ('mongodb://127.0.0.1:27017/yelp-camp')
     // userNewUrlParser: true, 
     // UseCreateIndex: true,
@@ -44,44 +46,48 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync (async (req, res, next) => {
 
 // In order for the req.body to work you need
 // app.use(express.urlencoded({extended: true}));
-// Already done 
-
+// Already done  
     const campground = new Campgound(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+   
+}))
 
 // Form to edit campgrounds
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync (async (req, res) => {
     const {id} = req.params;
     const campground = await Campgound.findById(id);
     res.render('campgrounds/edit', {campground})
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     const campground = await Campgound.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}))
 
 
 //Show Campground by ID
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync (async (req, res) => {
     const {id} = req.params;
     const campground = await Campgound.findById(id);
     res.render('campgrounds/show', {campground})
-})
+}))
 
 // Deleting Campground
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync (async (req, res) => {
     const {id} = req.params;
     const campground = await Campgound.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.use((err, req, res, next) => {
+    res.send("Damn! There is an error man!")
 })
 
 app.listen(3000, () => {
